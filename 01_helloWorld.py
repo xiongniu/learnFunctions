734279bb0ef4a5fa94e0961819ab429fd14b22a5
Ella <bearalicious1@gmail.com>
2025-10-29 12:14:42 +1100

    Update 01_helloWorld.py

--- 01_helloWorld.py
+++ 01_helloWorld.py
@@ -5,3 +5,17 @@ def asciiHelloWorld():
     print("|  _  |  __/ | | (_) |   \\ V  V / (_) | |  | | (_| |")
     print("|_| |_|\\___|_|_|\\___/     \\_/\\_/ \\___/|_|  |_|\\__,_|")
 
+# def main():
+#     print ("test line")
+#     asciiHelloWorld()
+# main()
+
+def addTwoNumbers(a,b):
+    return a+b
+
+
+print ("test line")
+asciiHelloWorld()
+
+c = addTwoNumbers(5,7)
+print("5 +7=", c)
